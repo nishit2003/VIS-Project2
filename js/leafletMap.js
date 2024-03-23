29df07a4d146c8fb9d40597c5a471b7e3a604624
Jeremy Oosterbaan <jeremy.oosterbaan@pe-ltd.com>
2024-03-23 18:14:53 -0400

    More progress with the CSV data parsing & base leaflet map.

--- js/leafletMap.js
+++ js/leafletMap.js
@@ -3,16 +3,123 @@ class LeafletMap {
     // TODO: Add if necessary
 
     // Constructor
-    constructor() {
-        // TODO: Implement as necessary or remove?
+    /**
+     * Class constructor with basic configuration.
+     * @param {Object}
+     * @param {Array}
+     */
+    constructor(_config, _data) {
+        this.config = { parentElement: _config.parentElement, };
+        this.data = _data;
+
+        // various leaflet map bases
+        this.ESRI_URL = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}';    // ESRI
+        this.ESRI_ATTR = 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community';
+        this.TOPO_URL = 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png';     // TOPO
+        this.TOPO_ATTR = 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)';
+        this.THOUT_URL = 'https://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png?apikey={apikey}';     // Thunderforest Outdoors- requires key, so maybe don't use?
+        this.THOUT_ATTR = '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+        this.ST_URL = 'https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.{ext}';     // Stamen
+        this.ST_ATTR = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
     }
 
     // Class Methods
     initVis() {
-        let vis = this; // saves reference to the class to a locally-scoped variable
+        let vis = this;     // saves reference to the class to a locally-scoped variable
 
-        // TODO: Visualization initalization stuff goes here
+        // experimenting with the topographic map:
+        vis.base_layer = L.tileLayer(vis.topoUrl, {
+            id: 'thout-image',
+            attribution: vis.topoAttr,
+            ext: 'png'
+        });
+    
+        vis.theMap = L.map('map', {
+            center: [30, 0],
+            zoom: 2,
+            layers: [vis.base_layer]
+        });
+
+        // initialize svg for d3 to add to map
+        L.svg({clickable:true}).addTo(vis.theMap)   // we have to make the svg layer clickable
+        vis.overlay = d3.select(vis.theMap.getPanes().overlayPane)
+        vis.svg = vis.overlay.select('svg').attr("pointer-events", "auto")
+
+        // these are the city locations, displayed as a set of dots 
+        vis.Dots = vis.svg.selectAll('circle')
+            .data(vis.data)
+            .join('circle')
+            .attr("fill", "steelblue")
+            .attr("stroke", "black")
+            // Leaflet has to take control of projecting points. Here we are feeding the latitude and longitude coordinates to
+            // leaflet so that it can project them on the coordinates of the view. Notice, we have to reverse lat and lon.
+            // Finally, the returned conversion produces an x and y point. We have to select the the desired one using .x or .y
+            .attr("cx", d => vis.theMap.latLngToLayerPoint([d.latitude,d.longitude]).x)
+            .attr("cy", d => vis.theMap.latLngToLayerPoint([d.latitude,d.longitude]).y)
+            .attr("r", 3)
+            .on('mouseover', function(event, d) {
+                d3.select(this).transition()    // D3 selects the object we have moused over in order to perform operations on it
+                    .duration('150')    // how long we are transitioning between the two states
+                    .attr("fill", "red")    // change the fill
+                    .attr("r", 4);  // change radius
+                
+                // create a tool tip
+                d3.select("#tooltip")
+                    .style('opacity', 1)
+                    .style('z-index', 1000000)
+                    .html(
+                        `<div class="tooltip-label">
+                            <ul>
+                                <li>City Area: ${d.city_area}</li>
+                                <li>Date Documented: ${d.date_documented}</li>
+                            </ul>
+                        </div>`
+                    );
+            })
+            .on('mousemove', (event) => {
+                //position the tooltip
+                d3.select('#tooltip')
+                    .style('left', (event.pageX + 10) + 'px')   
+                    .style('top', (event.pageY + 10) + 'px');
+            })
+            .on('mouseleave', function() {  // function to add mouseover event
+                d3.select(this).transition()    // D3 selects the object we have moused over in order to perform operations on it
+                    .duration('150')  // how long we are transitioning between the two states
+                    .attr("fill", "steelblue")    // change the fill
+                    .attr('r', 3)     // change radius
+                d3.select('#tooltip').style('opacity', 0);  // turn off the tooltip
+            });
+
+        // handler here for updating the map, as you zoom in and out           
+        vis.theMap.on("zoomend", function() {
+            vis.updateVis();
+        });
     }
 
-    // TOOD: Add as necessary
+    updateVis() {
+        let vis = this;
+
+        //want to control the size of the radius to be a certain number of meters? 
+        vis.radiusSize = 3; 
+
+        // if( vis.theMap.getZoom > 15 ) {
+        //   metresPerPixel = 40075016.686 * Math.abs(Math.cos(map.getCenter().lat * Math.PI/180)) / Math.pow(2, map.getZoom()+8);
+        //   desiredMetersForPoint = 100; //or the uncertainty measure... =) 
+        //   radiusSize = desiredMetersForPoint / metresPerPixel;
+        // }
+    
+        // redraw based on new zoom- need to recalculate on-screen position
+        vis.Dots
+            .attr("cx", d => vis.theMap.latLngToLayerPoint([d.latitude,d.longitude]).x)
+            .attr("cy", d => vis.theMap.latLngToLayerPoint([d.latitude,d.longitude]).y)
+            .attr("r", vis.radiusSize);
+    }
+
+    renderVis() {
+        let vis = this;
+
+        // not using right now...
+    }
+
+    // TOOD: Add class methods as necessary
 }
--- js/main.js
+++ js/main.js
@@ -1,27 +1,26 @@
-/// This script will act as the main "runner" of the entire application.
-///     For a modular layout, we could place all functionality in external scripts/classes, and simply instantiate + use objects here.
-///     I've created an example of this for the CsvDataParsing & DataStore classes.
-///     However, we don't have to use this layout by any means.
-///     I just wanted to add my thoughts & a sort of 'template' for us that'll keep things organized.
+/* This script will act as the main "runner" of the entire application. */
 
-// first of all, we parse the UFO .csv file data
-CsvDataParser.parseUfoData();
-
-// next we'll initialize the DataStore class.
-//  This includes filters, raw/filtered data, etc. Basically save all the data here
-
-// next we can generate the leafletMap
-let leafletMap = new LeafletMap();
-leafletMap.initVis();
-
-// next we can initialize and connect callbacks/listeners for other features we want to add (timeline, buttons, dropdowns, etc)
-//   Some examples in pseudocode:
-//var attributeDropdown = Dropdown()
-//var timelineSlider = TimelineSlider()
-//var resetButton = ResetButton()
-/*
-resetButton.addEventListener("click", function() {
-    // button that resets something
+// Because we've moved the CSV data parsing into a separate module, we need to ensure the rest of the program waits for CSV parsing to complete.
+//  If you look at the in-class examples, most of the visualization creation is done INSIDE of the "d3.csv()" tag, so the synchronization is encapsulated.
+//  Since we've split it out, we need to ensure the main runner waits for the parsing to complete. That is why I've launched the application in this way.
+async function main() {
+    await CsvDataParser.parseUfoData();     // parses UFO data & saves to the DataStore() class (singleton, static class)
+    
+    // next we can generate the leaflet map
+    let leafletMap = new LeafletMap({ parentElement: '#map'}, DataStore.filteredData);
+    leafletMap.initVis();
+    
+    // TODO: next we can initialize and connect callbacks/listeners for other features we want to add (timeline, buttons, dropdowns, etc)
+    //   Some examples in pseudocode:
+    //var attributeDropdown = Dropdown()
+    //var timelineSlider = TimelineSlider()
+    //var resetButton = ResetButton()
+    /*
+    resetButton.addEventListener("click", function() {
+        // button that resets something
+    }
+    .catch(error => console.error(error));
+    */
 }
-.catch(error => console.error(error));
-*/
+
+main(); // calls the main() function to begin execution
