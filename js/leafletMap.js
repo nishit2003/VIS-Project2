9560dd2f9d88b5d55994d07c08b4f6c82b65cec0
Johnathan Thompson <Thomp5je@mail.uc.edu>
2024-03-25 00:31:07 -0400

    4 colors Changing

--- js/leafletMap.js
+++ js/leafletMap.js
@@ -8,9 +8,10 @@ class LeafletMap {
      * @param {Object}
      * @param {Array}
      */
-    constructor(_config, _data) {
+    constructor(_config, _data, _filter) {
         this.config = { parentElement: _config.parentElement, };
         this.data = _data;
+        this.filter = _filter;
 
         // various leaflet map bases
         this.ESRI_URL = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}';    // ESRI
@@ -93,7 +94,6 @@ class LeafletMap {
             .on('mouseleave', function() {  // function to add mouseover event
                 d3.select(this).transition()    // D3 selects the object we have moused over in order to perform operations on it
                     .duration('150')  // how long we are transitioning between the two states
-                    .attr("fill", "steelblue")    // change the fill
                     .attr('r', 3)     // change radius
                 d3.select('#tooltip').style('opacity', 0);  // turn off the tooltip
             });
@@ -105,18 +105,12 @@ class LeafletMap {
     }
 
     updateVis() {
+        console.log("hi")
         let vis = this;
-        
+        vis.getColorScale(vis.filter)
         //want to control the size of the radius to be a certain number of meters? 
-        vis.radiusSize = 3; 
+        vis.radiusSize = 3;
 
-        // if( vis.theMap.getZoom > 15 ) {
-        //   metresPerPixel = 40075016.686 * Math.abs(Math.cos(map.getCenter().lat * Math.PI/180)) / Math.pow(2, map.getZoom()+8);
-        //   desiredMetersForPoint = 100; //or the uncertainty measure... =) 
-        //   radiusSize = desiredMetersForPoint / metresPerPixel;
-        // }
-    
-        // redraw based on new zoom- need to recalculate on-screen position
         vis.Dots
             .attr("cx", d => {
                 if ((d.longitude == "NA") || (d.latitude == "NA")) { /* If longitude/latitude is 'NA' then we do nothing */ }
@@ -126,8 +120,6 @@ class LeafletMap {
                 if ((d.longitude == "NA") || (d.latitude == "NA")) { /* If longitude/latitude is 'NA' then we do nothing */ }
                 else { return vis.theMap.latLngToLayerPoint([d.latitude, d.longitude]).y }
             })
-            //.attr("cx", d => vis.theMap.latLngToLayerPoint([d.latitude,d.longitude]).x)
-            //.attr("cy", d => vis.theMap.latLngToLayerPoint([d.latitude,d.longitude]).y)
             .attr("r", vis.radiusSize);
     }
 
@@ -137,5 +129,74 @@ class LeafletMap {
         // not using right now...
     }
 
+    getColorScale(filter) {
+
+        if (filter == "year") {
+            console.log("hi")
+            this.colorScale = d3.scaleOrdinal()
+                .range(["#585661", '#9151a8', '#85152f', '##b0522a', "#b09a2a",  "#186e26",  "#094263"])
+                .domain(['1950','1960',"1970","1980","1990", "2000", "2010"]);
+            this.Dots.attr("fill", d => {
+                if (typeof d.date_time != "number") {
+                    var year = d.date_time.split(" ")[0].split("/")[2]
+                    if (Number(year) <= 1959) {return this.colorScale("1950")}
+                    else if (Number(year) <= 1969) {return this.colorScale("1960")}
+                    else if (Number(year) <= 1979) {return this.colorScale("1970")}
+                    else if (Number(year) <= 1989) {return this.colorScale("1980")}
+                    else if (Number(year) <= 1999) {return this.colorScale("1990")}
+                    else if (Number(year) <= 2009) {return this.colorScale("2000")}
+                    else if (Number(year) <= 2019) {return this.colorScale("2010")}
+                }
+                else {return this.colorScale("2010")}
+            })
+        }
+        else if (filter == "month") {
+            this.colorScale = d3.scaleOrdinal()
+            .range(['#c41d1d', '#995f12', '#998c12', "#7a9912", "#3b9912", "#0f7d1c", "#0f7d41", "#0f7d63", "#0f787d", "#0e75a1", "#0f4187", "#0f2387"])
+            .domain(['1','2', "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]);
+            this.Dots.attr("fill", d => {
+                if (typeof d.date_time != "number") {
+                    var month = d.date_time.split(" ")[0].split("/")[0]
+                    if (Number(month) == 1) {return this.colorScale("1")}
+                    else if (Number(month) == 2) {return this.colorScale("2")}
+                    else if (Number(month) == 3) {return this.colorScale("3")}
+                    else if (Number(month) == 4) {return this.colorScale("4")}
+                    else if (Number(month) == 5) {return this.colorScale("5")}
+                    else if (Number(month) == 6) {return this.colorScale("6")}
+                    else if (Number(month) == 7) {return this.colorScale("7")}
+                    else if (Number(month) == 8) {return this.colorScale("8")}
+                    else if (Number(month) == 9) {return this.colorScale("9")}
+                    else if (Number(month) == 10) {return this.colorScale("10")}
+                    else if (Number(month) == 11) {return this.colorScale("11")}
+                    else if (Number(month) == 12) {return this.colorScale("12")}
+                }
+                else {return this.colorScale("1")}
+            })
+        }
+        else if (filter == "ufo_shape") {
+            this.colorScale = d3.scaleOrdinal()
+            .range(['#c41d1d', '#995f12', '#998c12', "#7a9912", "#3b9912", "#0f7d1c", "#0f7d41", "#0f7d63", "#0f787d", "#0e75a1",
+                    "#0f4187", "#0f2387", "#321bb3", "#3f1170", "#9c3cc2", "#744975", "#c286b4", "#752b49", "#8f2845", "#8f2845"])
+            .domain(['changing','chevron', "cigar", "circle", "cylinder", "diamond", "disk", "egg", "fireball", "flash", 
+                    "formation", "light", "NA", "other", "oval", "rectangle", "sphere", "teardrop", "triangle", "unknown"]);
+            this.Dots.attr("fill", d => this.colorScale(d.ufo_shape))
+        }
+        else if (filter == "time_day") {
+            this.colorScale = d3.scaleOrdinal()
+            .range(["#19544e", '#0f780d', '#83b010', '#b04010'])
+            .domain(['0:00','6:00', "12:00", "18:00"]);
+            this.Dots.attr("fill", d => {
+                if (typeof d.date_time != "number") {
+                    var hour = d.date_time.split(" ")[1].split(":")[1]
+                    if (Number(hour) <= 5 || Number(hour) >= 22) {return this.colorScale("0:00")}
+                    else if (Number(hour) >= 6 && Number(hour) <= 10) {return this.colorScale("6:00")}
+                    else if (Number(hour) >= 11 && Number(hour) <= 17) {return this.colorScale("12:00")}
+                    else if (Number(hour) >= 18 && Number(hour) <= 21) {return this.colorScale("18:00")}
+                }
+                else {return this.colorScale("0:00")}
+            })
+        }
+        else {this.Dots.attr("fill", "blue")}
+    }
     // TOOD: Add class methods as necessary
 }
--- js/main.js
+++ js/main.js
@@ -4,11 +4,18 @@
 //  If you look at the in-class examples, most of the visualization creation is done INSIDE of the "d3.csv()" tag, so the synchronization is encapsulated.
 //  Since we've split it out, we need to ensure the main runner waits for the parsing to complete. That is why I've launched the application in this way.
 async function main() {
-    await CsvDataParser.parseUfoData();     // parses UFO data & saves to the DataStore() class (singleton, static class)
+    await CsvDataParser.parseUfoData(); 
     
     // next we can generate the leaflet map
-    let leafletMap = new LeafletMap({ parentElement: '#map'}, DataStore.filteredData);
+    let leafletMap = new LeafletMap({ parentElement: '#map'}, DataStore.filteredData, "");
     leafletMap.initVis();
+    console.log(leafletMap)
+
+    document.getElementById("toggleBtn").addEventListener("click", function() {
+        var filter = document.getElementById("filter").value;
+        leafletMap.filter = filter;
+        leafletMap.updateVis();
+    });
     
     // TODO: next we can initialize and connect callbacks/listeners for other features we want to add (timeline, buttons, dropdowns, etc)
     //   Some examples in pseudocode:
